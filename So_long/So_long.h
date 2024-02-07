7eed36e7c37857e63d92ec1c69f95078ee8c39c4
Mossab Errahal <merrahal@e2r3p12.1337.ma>
2024-02-07 11:23:01 +0100

    so_long

--- So_long/So_long.h
+++ So_long/So_long.h
@@ -5,6 +5,17 @@
 #include <unistd.h>
 #include <mlx.h>
 #include <stdlib.h>
+#include "ft_printf/ft_printf.h"
+
+
+//constants values
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//used functions
+int	ft_printf(const char *str, ...);
+
+
 
 typedef struct  s_var
 {
@@ -12,5 +23,12 @@ typedef struct  s_var
     void    *win;
 }               t_var;
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}   img_data;
 
 #endif
--- So_long/main.c
+++ So_long/main.c
@@ -5,36 +5,54 @@ int my_func(int key, void *ptr)
     printf("the key pressed is  --> %d\n", key);
     return (0);
 }
-
-int main()
+void	my_mlx_pixel_put(img_data *data, int x, int y, int color)
 {
-    t_var   data;
+	char	*dst;
 
-    // initialize minilibX
-    data.mlx = mlx_init();
-    //create a window
-    data.win = mlx_new_window(data.mlx, 800, 600, "my first window");
-
-    int x,y;
-    y = 50;
-    x = 50;
-    while(x < 300)
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+int main(int ac, char *av[])
+{
+    t_var       data;
+    img_data    img;
+    if (ac == 1)
     {
-        while (y < 600)
+        // initialize minilibX
+        /*mlx_init*/
+        data.mlx = mlx_init();
+        if(!(data.mlx))
         {
-            mlx_pixel_put(data.mlx, data.win, x, y, 0xFF6600);
-            y++;
+            ft_printf("intialization problem in --> {mlx_init}\n");
+            return (1);
         }
-        y = 50;
-        x++;
-    }    
-    mlx_string_put(data.mlx, data.win, 100, 200, 0x0000FF, "Hello, MinilibX!");
-    // displays the window and keeps it open
-    mlx_key_hook(data.win, my_func, NULL);
-    
-    
-    
-    system("leaks solong");
-    mlx_loop(data.mlx);
+        
+        //create a window
+        /*mlx_new_window*/
+        data.win = mlx_new_window(data.mlx, WIN_WIDTH, WIN_HEIGHT, "my first window");
+        if(!(data.win))
+        {
+            ft_printf("problem during opening the window in --> {mlx_new_window}\n");
+            return (1);
+        }
+        
+        //create and initialize a new image
+        img.img = mlx_new_image(data.mlx, 800, 600);
+        // put the image informations in the struct
+        img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+        my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
+        mlx_put_image_to_window(data.mlx, data.win, img.img, 0, 0);
+
+    //     /*mlx_string_put*/
+    //     //mlx_string_put(data.mlx, data.win, 100, 200, 0x0000FF, "Hello, MinilibX!");
+
+    //     // displays the window and keeps it open
+    //     /*mlx_key_hook*/
+    //     mlx_key_hook(data.win, my_func, 0);
+
+    //     system("leaks solong");
+    //     /*mlx_loop*/
+        mlx_loop(data.mlx);
+    }
     return (0);
 }
