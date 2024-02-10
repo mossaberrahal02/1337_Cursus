c64aa40f609ab1a3b886dc553486407a4abf8394
Mossab Errahal <merrahal@e2r4p12.1337.ma>
2024-02-10 21:56:23 +0100

    so_long

--- So_long/So_long.h
+++ So_long/So_long.h
@@ -1,34 +1,47 @@
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <mlx.h>
-#include <stdlib.h>
-#include "ft_printf/ft_printf.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <mlx.h>
+# include <stdlib.h>
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
 
 
 //constants values
-#define WIN_WIDTH 800
-#define WIN_HEIGHT 600
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 600
 
 //used functions
 int	ft_printf(const char *str, ...);
 
 
 
-typedef struct  s_var
+typedef struct  s_mlx
 {
     void    *mlx;
     void    *win;
-}               t_var;
+}               t_mlx;
 
-typedef struct	s_data {
+typedef struct	s_img {
 	void	*img;
-	char	*addr;
+	int		*addr; //this was a char* and i think this should be int* 
+	/*
+	 Now the important part :
+	 mlx_get_data_addr will return a char* that is 4 times the (width * height) of your image.
+	 Why so ? Let me explain : This char* will represent your image, pixel by pixel,
+	 and the values of this array are the colors. That's why the array is 4 times bigger :
+	 you need 4 char to code the color of each pixels (one for Red, Green and Blue) and one for the alpha.
+	 But... it's not very convenient, right ? So here is my little trick : you cast
+	 mlx_get_data_addr as an int* and store it in an int*.
+	 This way, the array will have the exact same size as your window, and each index
+	 will represent one complete color of a pixel !
+	*/   /*!!!!!!!!!!!       //img.addr = (int *)mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);// check the header to understand why i cast!!!!!*/
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}   img_data;
+}   t_img;
 
 #endif
--- So_long/main.c
+++ So_long/main.c
@@ -1,58 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merrahal <merrahal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 21:23:36 by merrahal          #+#    #+#             */
+/*   Updated: 2024/02/10 21:43:29 by merrahal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "So_long.h"
 
-int my_func(int key, void *ptr)
+int has_only_walls(char **arr)
 {
-    printf("the key pressed is  --> %d\n", key);
+     
+}
+
+static size_t   ft_strlen_no_new_line(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	return (i);
+}
+
+static void path_checker(char *path)
+{
+    size_t i = ft_strlen(path);//maps/.ber
+
+    if (path[i - 1] != 'r' || path[i - 2] != 'e' || path[i - 3] != 'b' || path[i - 4] != '.' || path[i - 5] == '/')
+    {
+        ft_printf("--path is not valid--\n");
+        exit(1);
+    }
+}
+static void map_h_w_checkWidth(char *line, int fd, size_t *nbr_h_map, size_t *nbr_w_map)
+{
+    size_t height = 1;
+    free(line);
+    while ((line = get_next_line(fd)))
+    {
+        height++;
+        if(ft_strlen_no_new_line(line) != *nbr_w_map)
+        {
+            free(line);
+            ft_printf("unvalid MAP here");
+            exit(1);
+        }
+        free(line);
+    }
+    *nbr_h_map = height;
+}
+
+void copy_map(char *path, size_t nbr_lines)
+{
+    int fd = open("./maps/map1.ber", O_RDONLY);
+    if(fd < 0)
+    {
+        ft_printf("\nma thelch l file dyal l map men l fnc copy_map()\n");
+        exit(1);
+    }
+    char **arr = malloc((nbr_lines + 1) * sizeof(char *));
+    if (!arr)
+    {
+        ft_printf("\nfailed allocation from copy_map()\n");
+        exit(1);
+    }
+    char *line;
+    int i = 0;
+    while ((line = get_next_line(fd)))
+    {
+        arr[i] = ft_strdup(line);
+        // arr[i] = line;
+        strcpy(arr[i], line);
+        i++;
+        free(line);
+    }
+    arr[i] = NULL;
+// khas n check wash l map dayrin biha l walls aka 1
+    has_only_walls(arr);
+    int j = 0;
+    while (arr[j])
+    {
+        // ft_printf("\nline %d  --> %s\n", j, arr[j]);
+        free(arr[j]);//hmm should i do it right now
+        j++;
+    }
+    free(arr);//hmm should i do it right now
+}
+
+static int map_checker(char *path, t_mlx *data)
+{
+    path_checker(path);
+    int fd = open("./maps/map1.ber", O_RDONLY);
+    if(fd < 0)
+    {
+        ft_printf("ma thelch l file dyal l map");
+        exit(1);
+    }
+    char *line = get_next_line(fd);
+    if(!line)
+    {
+        ft_printf("unvalid MAP");
+        exit(1);
+    }
+    size_t nbr_h_map;
+    size_t nbr_w_map;
+    nbr_w_map = ft_strlen_no_new_line(line);
+    map_h_w_checkWidth(line, fd, &nbr_h_map, &nbr_w_map);
+    copy_map(path, nbr_h_map);
     return (0);
 }
-void	my_mlx_pixel_put(img_data *data, int x, int y, int color)
-{
-	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
 int main(int ac, char *av[])
 {
-    t_var       data;
-    img_data    img;
-    if (ac == 1)
+    t_mlx   data;
+    t_img   img;//useles till now 
+    if (ac == 2)
     {
-        // initialize minilibX
-        /*mlx_init*/
-        data.mlx = mlx_init();
-        if(!(data.mlx))
-        {
-            ft_printf("intialization problem in --> {mlx_init}\n");
-            return (1);
-        }
-        
-        //create a window
-        /*mlx_new_window*/
-        data.win = mlx_new_window(data.mlx, WIN_WIDTH, WIN_HEIGHT, "my first window");
-        if(!(data.win))
-        {
-            ft_printf("problem during opening the window in --> {mlx_new_window}\n");
-            return (1);
-        }
-        
-        //create and initialize a new image
-        img.img = mlx_new_image(data.mlx, 800, 600);
-        // put the image informations in the struct
-        img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-        my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
-        mlx_put_image_to_window(data.mlx, data.win, img.img, 0, 0);
-
-    //     /*mlx_string_put*/
-    //     //mlx_string_put(data.mlx, data.win, 100, 200, 0x0000FF, "Hello, MinilibX!");
-
-    //     // displays the window and keeps it open
-    //     /*mlx_key_hook*/
-    //     mlx_key_hook(data.win, my_func, 0);
-
-    //     system("leaks solong");
-    //     /*mlx_loop*/
-        mlx_loop(data.mlx);
+        map_checker(av[1], &data);
     }
-    return (0);
+    else
+    {
+        ft_printf("many or less main arguments");
+        exit(1);
+    }
+
+
+    // system("leaks solong");
+    return (0); 
 }
