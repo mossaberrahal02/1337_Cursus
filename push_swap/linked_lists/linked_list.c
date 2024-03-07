1aba4dc4007075e9138d2a189627243f77b4763a
mossaberrahal02 <mossaberrahal@gmail.com>
2024-03-07 11:36:31 +0100

    1337

--- push_swap/linked_lists/linked_list.c
+++ push_swap/linked_lists/linked_list.c
@@ -15,27 +15,43 @@ t_list * ft_lstnew(void *content)
     return(new);
 }
 
-t_list * ft_lstadd_front(t_list **lst, t_list *new)
+void ft_lstadd_front(t_list **lst, t_list *new)
 {
     new->next = *lst;
-    return(new);
+    *lst = new;
 }
 
-int main() {
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+    t_list *tmp = *lst;//address dyal head
+    while (tmp->next != NULL)
+        tmp = tmp->next;
+    tmp->next = new;
+}
 
+int main()
+{
+    t_list  *head = NULL;
     t_list *first = ft_lstnew("first \n");
     t_list *seconde = ft_lstnew("seconde\n");
     t_list *third = ft_lstnew("third\n");
-    ft_lstadd_front(&first, seconde);
-    ft_lstadd_front(&seconde, third);
-    printf("%s", first->content);
-    printf("%s", seconde->content);
-    printf("%s", third->content);
-    while (first)
-    {
-        printf("-->%s\n",first->content);
-        first = first->next;
-    }
-    
+    t_list *ana = ft_lstnew("ana\n");
+
+    ft_lstadd_front(&head, first);   //first->next = null    head = first
+    // printf("head = %p &first = %p \n", head, first);
+    ft_lstadd_front(&head, seconde); //second->next = first  head = seconde
+    // printf("head = %p &seconde = %p \n", head, seconde);
+    ft_lstadd_front(&head, third); //third->next = seconde  head = third
+    // printf("head = %p third = %p \n", head, third);
+
+    // while (head != NULL)
+    // {
+    //     printf("-->%s\n",head->content);
+    //     head = head->next;
+    // }
+    // printf("-->%d\n",count);
+
+    ft_lstadd_back(&head, ana);
+
     return 0;
 }
