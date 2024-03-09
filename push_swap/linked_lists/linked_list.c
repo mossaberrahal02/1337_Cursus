7197b52685bb6e0a40aba167b119e746b22bddf7
mossaberrahal02 <mossaberrahal@gmail.com>
2024-03-09 03:12:57 +0100

    lll

--- push_swap/linked_lists/linked_list.c
+++ push_swap/linked_lists/linked_list.c
@@ -15,42 +15,86 @@ t_list *ft_lstnew(void *content)
     return (new);
 }
 
-
-void ft_lstadd_frontttt(t_list **lst, t_list *new)
-{
-    if (!(*lst))
-    {
-        *lst = new;
-        return ;
-    }
-    new->next = *lst;
-    *lst = new;
-}
-
-
-void ft_lstadd_front(t_list **lst, t_list* new)
+void ft_addfront(t_list **lst, t_list *new)
 {
     if(!(*lst))
     {
+        (*lst) = new;
+        return ;
+    }
+    new->next = (*lst);
+    (*lst) = new;
+}
+
+int ft_lstsize(t_list *lst)
+{
+    t_list *tmp = lst;
+    int i = 0;
+    while(tmp->next)
+    {
+        i++;
+        tmp = tmp->next;
+    }
+    return i;
+}
+
+t_list* ft_lstlast(t_list *lst)
+{
+    t_list *tmp = lst;
+    while(tmp->next)
+        tmp = tmp->next;
+    return (tmp);
+}
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+    if(*lst == NULL)
+    {
         *lst = new;
         return ;
     }
-    printf("here\n");
-    (*lst)->next = new;
-    *lst = new;
+    t_list *tmp = *lst;
+    while (tmp->next)
+        tmp = tmp->next;
+    tmp->next = new;
+}
+
+/*
+while(tmp)
+{
+    tmp = tmp->next;               <------   hadi bach t iteri 3la ga3 nodes li f liste
+}
+while(tmp->next)
+{
+    tmp = tmp->next;           <------  hadi ka tkon met2eked bli first node exists and
+                                        t iteri 3la ga3 nodes exept the last one gh tkon excluded men l boucle
+}
+*/
+
+
+void ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+     
 }
 
 int	main(void)
 {
     t_list *head = NULL;
-    t_list *mossab = ft_lstnew("mossab\n");
-    t_list *ayoub = ft_lstnew("ayoub\n");
-    ft_lstadd_front(&head, mossab);
-    ft_lstadd_front(&head, ayoub);
-    while (head)
-    {
-        printf("--> %s  %p", head->content, head);
-        head=head->next;
-    }
+    t_list *mossab = ft_lstnew("mossab");
+    t_list *afnane = ft_lstnew("afnane");
+    t_list *alo = ft_lstnew("alo");
+    t_list *iamlast = ft_lstnew("iamlast");
+    ft_addfront(&head, mossab);
+    ft_addfront(&head, afnane);
+    ft_addfront(&head, alo);
+
+    ft_lstadd_back(&head, iamlast);
+    // while(head)
+    // {
+    //     printf("%s\n", head->content);
+    //     head = head->next;
+    // }
+    t_list *last = ft_lstlast(head);
+    printf("here the last == %s\n",last->content);
 	return (0);
 }
